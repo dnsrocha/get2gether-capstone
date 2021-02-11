162826d45227ff6cc9234b09110b628743fa57f4
dnsrocha <drocha@gmail.com>
2021-02-10 21:09:15 -0800

    added private route

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,21 @@
 import React from 'react' 
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
 
 
 export default function PrivateRoute ({ component: Component, ...rest}) {
-    return (
-        <div>
 
-        </div>
+    const { currentUser } = useAuth()
+
+    return (
+        <Route
+        {...rest}
+        render={props => {
+            currentUser ? <Component {...props} /> : <Redirect to="/login" />
+        }}
+        >
+
+
+        </Route>
     )
 }
